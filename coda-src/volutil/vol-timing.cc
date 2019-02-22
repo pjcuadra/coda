b829e91f66f69b675b3f001b30c0e8ff460f365b
Pedro Cuadra <pcuadrac@cs.cmu.edu>
2019-02-22 16:08:01 -0500

    Remove timing library as PROBE is never called

--- coda-src/volutil/vol-timing.cc
+++ coda-src/volutil/vol-timing.cc
@@ -40,7 +40,6 @@ extern "C" {
 #include <vice.h>
 #include <cvnode.h>
 #include <volume.h>
-#include <timing.h>
 #include <util.h>
 
 extern int probingon;
@@ -55,58 +54,5 @@ static FILE *timingfile;
 long S_VolTiming(RPC2_Handle rpcid, RPC2_Integer OnFlag,
                  SE_Descriptor *formal_sed)
 {
-    SE_Descriptor sed;
-    int rc = 0;
-
-    LogMsg(9, VolDebugLevel, stdout, "Entering S_VolTiming: OnFlag = %d",
-           OnFlag);
-
-    VInitVolUtil(volumeUtility);
-    if (OnFlag) {
-        probingon = 1;
-        LogMsg(9, VolDebugLevel, stdout, "Probing is now %s",
-               probingon ? "ON" : "OFF");
-    } else if (probingon) {
-        probingon  = 0;
-        timingfile = fopen(TIMINGFILE, "w");
-        fprintf(timingfile, "Processing tpinfo and FileresTPinfo \n");
-        /* process the buffer */
-        if (Gettpinfo()) {
-            Gettpinfo()->postprocess(timingfile);
-            Cleartpinfo();
-        }
-        if (GetFileresTPinfo()) {
-            GetFileresTPinfo()->postprocess(timingfile);
-            ClearFileresTPinfo();
-        }
-        fprintf(timingfile, "Finished processing tpinfo and FileresTPinfo \n");
-        fclose(timingfile);
-
-        /* set up SE_Descriptor for transfer */
-        memset(&sed, 0, sizeof(SE_Descriptor));
-        sed.Tag                                   = SMARTFTP;
-        sed.Value.SmartFTPD.Tag                   = FILEBYNAME;
-        sed.Value.SmartFTPD.TransmissionDirection = SERVERTOCLIENT;
-        strcpy(sed.Value.SmartFTPD.FileInfo.ByName.LocalFileName, TIMINGFILE);
-        sed.Value.SmartFTPD.FileInfo.ByName.ProtectionBits = 0755;
-
-        if ((rc = RPC2_InitSideEffect(rpcid, &sed)) <= RPC2_ELIMIT) {
-            LogMsg(0, VolDebugLevel, stdout,
-                   "VolTiming: InitSideEffect failed with %s",
-                   RPC2_ErrorMsg(rc));
-            VDisconnectFS();
-            return (-1);
-        }
-
-        if ((rc = RPC2_CheckSideEffect(rpcid, &sed, SE_AWAITLOCALSTATUS)) <=
-            RPC2_ELIMIT) {
-            LogMsg(0, VolDebugLevel, stdout,
-                   "VolTiming: CheckSideEffect failed with %s",
-                   RPC2_ErrorMsg(rc));
-            VDisconnectFS();
-            return (-1);
-        }
-    }
-    VDisconnectFS();
-    return (0);
+    return RPC2_INVALIDOPCODE;
 }
